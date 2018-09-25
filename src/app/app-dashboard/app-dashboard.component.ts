a83e8c4b6f7cd0a63296aea42808320356be5e31
OJAS-IT\chrakesh <rakesh.chengelli@ojas-it.com>
2018-09-25 13:00:25 +0530

    Change

--- src/app/app-dashboard/app-dashboard.component.ts
+++ src/app/app-dashboard/app-dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-app-dashboard',
-  template: `<h1 align="center">DashBoArd Displayed..... Changed For the some time even if it !</h1>`,
+  template: `<h1 align="center">DashBoArd Displayed..... Changed Rakesh For the some time even if it !</h1>`,
   styles: []
 })
 export class AppDashboardComponent implements OnInit {
