1944fa66e87abbb56c7170d37749caae99601583
OJAS-IT\chrakesh <rakesh.chengelli@ojas-it.com>
2018-09-25 12:52:55 +0530

    hello dashboard changed

--- src/app/app-dashboard/app-dashboard.component.ts
+++ src/app/app-dashboard/app-dashboard.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { WarningService } from '../warning.service';
 
 @Component({
   selector: 'app-app-dashboard',
-  template: `<h1 align="center">Dashboard.....! dashboard dashboard!</h1>`,
+  template: `<h1 align="center">Dashboard.....!</h1>`,
   styles: []
 })
 export class AppDashboardComponent implements OnInit {
