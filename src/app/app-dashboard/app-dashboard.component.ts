7701e1e67f34a8e010cc2840fb94e878a838fa3a
OJAS-IT\chrakesh <rakesh.chengelli@ojas-it.com>
2018-09-25 12:44:13 +0530

    Dashboard page added

--- src/app/app-dashboard/app-dashboard.component.ts
+++ src/app/app-dashboard/app-dashboard.component.ts
@@ -3,7 +3,7 @@ import { WarningService } from '../warning.service';
 
 @Component({
   selector: 'app-app-dashboard',
-  template: `<h1 align="center">Dashboard.....!</h1>`,
+  template: `<h1 align="center">Dashboard.....! dashboard dashboard!</h1>`,
   styles: []
 })
 export class AppDashboardComponent implements OnInit {
