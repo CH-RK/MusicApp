385a73e742e41e480f88bf7bf0502cc72228c765
OJAS-IT\chrakesh <rakesh.chengelli@ojas-it.com>
2018-09-25 12:40:14 +0530

    Music App Done With CanActivate and registration and Login Also Done

---
+++ src/app/app-dashboard/app-dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { WarningService } from '../warning.service';
+
+@Component({
+  selector: 'app-app-dashboard',
+  template: `<h1 align="center">Dashboard.....!</h1>`,
+  styles: []
+})
+export class AppDashboardComponent implements OnInit {
+  constructor(){
+    
+  }
+  ngOnInit() {
+  }
+
+}
